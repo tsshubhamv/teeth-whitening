c4242ca9762f2333f1de095e8bda704791934e7d
Shubham Vishwakarma <shubham.vishwakarma@toothsi.in>
2023-06-22 08:58:33 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import torch
+import cv2
+import torchvision
+import wandb
+import visdom
+import dominate
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(torch.__version__)
+    print(cv2.version.opencv_version)
+    print(torchvision.__version__)
+    print(wandb.__version__)
+    print(dominate.__version__)
+    print(visdom)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
